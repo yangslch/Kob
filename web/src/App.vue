9d0462576fa5d3ad1ae1fc554698bf01a2f5462c
conghuiyang <997370457@qq.com>
2022-12-05 12:07:08 +0800

    git初始

---
+++ web/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>  
+</template>
+
+<script>
+
+</script>
+
+<!--style用来设置样式
+-->
+<style>
+
+body {
+  background: url("@/assets/image/background.png");
+  background-size: cover;
+}
+
+</style>
